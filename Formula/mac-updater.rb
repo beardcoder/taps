6df5b1aaa8196c083bd45fce4190cfc61cc4c85b
Markus Sommer <markus.sommer@fullhaus.de>
2025-05-08 12:11:15 +0200

    Update mac-updater version to 0.3.1-1

--- Formula/mac-updater.rb
+++ Formula/mac-updater.rb
@@ -6,7 +6,7 @@ class MacUpdater < Formula
   homepage "https://github.com/beardcoder/mac-updater"
   url "https://github.com/beardcoder/mac-updater/releases/download/v0.3.1/mac-updater.zip"
   sha256 "08eadac29b021ca27f308f4d56aa8896d56c34ff260b2c51b89b7711e0c9a033"
-  version "0.3.1"
+  version "0.3.1-1"
 
   def install
     bin.install "mac-updater"
