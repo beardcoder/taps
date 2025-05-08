ca5cd45b7f229f8d64c05b0f7b835fc5b20d20f6
Markus Sommer <markus.sommer@fullhaus.de>
2025-05-08 12:07:52 +0200

    Update mac-updater to version 0.3.1 with new URL and SHA256 checksum

--- Formula/mac-updater.rb
+++ Formula/mac-updater.rb
@@ -4,9 +4,9 @@
 class MacUpdater < Formula
   desc "A tool to update macOS applications"
   homepage "https://github.com/beardcoder/mac-updater"
-  url "https://github.com/beardcoder/mac-updater/releases/download/v0.3.0/mac-updater.zip"
-  sha256 "aaa0ebbaad7146132b2fa6e32340a813a809c43edec7f0ec8007a3cb11c2bcc4"
-  version "0.3.0"
+  url "https://github.com/beardcoder/mac-updater/releases/download/v0.3.1/mac-updater.zip"
+  sha256 "08eadac29b021ca27f308f4d56aa8896d56c34ff260b2c51b89b7711e0c9a033"
+  version "0.3.1"
 
   def install
     bin.install "mac-updater"
